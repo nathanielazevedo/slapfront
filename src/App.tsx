d9d817a9a99f5b957454e3a1b59cd30131fb102b
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-11-05 22:32:05 -0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [message, setMessage] = useState()
+
+  async function slap(){
+    let result = await axios.get('https://slaphim.herokuapp.com/addslap');
+    setMessage(result.data.message)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>{message}</div>
+      <button onClick={slap}>Slap Him!</button>
     </div>
   );
 }
